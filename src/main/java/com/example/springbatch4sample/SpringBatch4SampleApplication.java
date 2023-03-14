220e5a1aaa3139e64100a053becfb40b57e0f4f4
Charles Aggasid <charles.aggasid01@gmail.com>
2023-03-14 18:59:55 -0500

    test

---
+++ src/main/java/com/example/springbatch4sample/SpringBatch4SampleApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbatch4sample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBatch4SampleApplication {
+	
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBatch4SampleApplication.class, args);
+	}
+	
+}
---
+++ src/test/java/com/example/springbatch4sample/SpringBatch4SampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbatch4sample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBatch4SampleApplicationTests {
+	
+	@Test
+	void contextLoads() {
+	}
+	
+}
